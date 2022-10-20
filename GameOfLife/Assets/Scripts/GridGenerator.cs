ca522002e8aa653b144b095d502cf4509e41aa72
KrymoXCIII <karym.yousfi@gmail.com>
2022-10-20 15:22:50 +0200

    Update GridGenerator.cs

--- GameOfLife/Assets/Scripts/GridGenerator.cs
+++ GameOfLife/Assets/Scripts/GridGenerator.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GridGenerator : MonoBehaviour
 {
+    private int currentFrame;
     private Entity[,] tab = new Entity[20,20];
 
     private Entity[,] tempTab = new Entity[20, 20];
@@ -17,64 +20,186 @@ public class GridGenerator : MonoBehaviour
         {
             for (int j = 0; j < tab.GetLength(1); j++)
             {
-                randomSelector = Random.Range(0, 4);
-
-                switch (randomSelector)
-                {
-                    case 0 :
-                        tab[i,j] = null;
-                        break;
-                    case 1 :
-                        tab[i, j] = new Wolf();
-                        break;
-                    case 2 :
-                        tab[i, j] = new Sheep();
-                        break;
-                    case 3 :
-                        tab[i, j] = new Grass();
-                        break;
-                } 
+                randomSelector = Random.Range(0, 3);
+                tab[i,j] = new Entity();
+                tab[i, j].entityType = randomSelector;
+                      
+                   
+                
             }
         }
-        
+
+        currentFrame = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentFrame++;
+
+            for (int i = 0; i < tab.GetLength(0); i++)
+            {
+                for (int j = 0; j < tab.GetLength(1); j++)
+                {
+                    // Type d'entité
+                    switch (tab[i, j].entityType)
+                    {
+                        case 0: // Grass
+                            
+                            // Reproduction 
+                            if (tab[i, j].isAlive)
+                            {
+                                List<Entity> entityListSheep = new List<Entity>();
+                                List<Entity> entityListWolf = new List<Entity>();
+                                // Verif cases 
+                                for (int x = -1; x < 2; x++)
+                                {
+                                    for (int y = -1; y < 2; y++)
+                                    {
+                                        if (x != 0 || y != 0)
+                                        {
+                                            if (x + i < tab.GetLength(0) && x + i > 0 && y + j < tab.GetLength(1) && y + j > 0)
+                                            {
+                                                if (tab[x + i,y + j].entityType == 2)
+                                                {
+                                                    entityListSheep.Add(tab[x + i,y + j]);
+                                                    
+                                                }
+
+                                                if (tab[x + i, y + j].entityType == 1)
+                                                {
+                                                    entityListWolf.Add(tab[x + i,y + j]);
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+
+                                if (entityListSheep.Count >= 2)
+                                {
+                                    tempTab[i, j] = new Entity(2);
+                                    break;
+
+                                }
+
+                                if (entityListWolf.Count >= 2)
+                                {
+                                    tempTab[i, j] = new Entity(1);
+                                    break;
+                                }
+
+                            }
+                            // Mort
+                            if (tab[i, j].isAlive == false)
+                            {
+                                tempTab[i, j] = new Entity(tab[i, j]);
+                                tempTab[i, j].timeBeforeRespawn--;
+
+                                if (tempTab[i, j].timeBeforeRespawn == 0)
+                                {
+                                    tempTab[i, j].isAlive = true;
+                                    tempTab[i, j].timeBeforeRespawn = 5;
+                                }
+
+
+
+                            }
+                            // Mangé ?
+                            if (tab[i, j].isAlive)
+                            {
+                                List<Entity> entityListG = new List<Entity>();
+                                // Verif cases 
+                                for (int x = -1; x < 2; x++)
+                                {
+                                    for (int y = -1; y < 2; y++)
+                                    {
+                                        if (x != 0 || y != 0)
+                                        {
+                                            if (x + i < tab.GetLength(0) && x + i > 0 && y + j < tab.GetLength(1) && y + j > 0)
+                                            {
+                                                if (tab[x + i,y + j].entityType == 2)
+                                                {
+                                                    entityListG.Add(tab[x + i,y + j]);
+                                                    
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+
+                                if (entityListG.Count != 0)
+                                {
+                                    Entity eater = entityListG[Random.Range(0, entityListG.Count)];
+                                    
+                                    eater.timeBeforeStarving = 5;
+
+                                    tab[i, j].timeBeforeRespawn = 5;
+                                    tab[i, j].isAlive = false;
+                                }
+
+                            }
+                            break;
+                        
+                        case 1 : // Wolf
+                            List<Entity> entityListW = new List<Entity>();
+                            
+
+
+
+                            break;
+                    }
+
+                }
+            }
+
+
+
+
+
+
+            tab = tempTab;
+        }
+
+
+
+
     }
+
+    
 }
 
 class Entity
 {
-    private bool isAlive;
+
+    public Entity(int entityType)
+    {
+        isAlive = true;
+        this.entityType = entityType;
+    }
     
+    public Entity(Entity entity)
+    { 
+        isAlive = entity.isAlive ;
+        entityType = entity.entityType; // 0 : Grass ; 1 : Wolf ; 2 : Sheep
+        timeBeforeStarving = entity.timeBeforeStarving;
+        lifeDuration = entity.lifeDuration;
+       
+        timeBeforeRespawn = entity.timeBeforeRespawn;
+    
+    }
+    public bool isAlive;
+    public int entityType; // 0 : Grass ; 1 : Wolf ; 2 : Sheep
+    public int timeBeforeStarving = 5;
+    public int lifeDuration = 20;
+    
+    public int timeBeforeRespawn = 5;
+    
+    
+
 }
 
-class Animal : Entity
-{
-    private int timeBeforeStarving = 5;
-    private int lifeDuration = 20;
-    private bool isFed;
-
-}
-class Wolf : Animal
-{
-    
-}
-
-class Sheep : Animal
-{
-    
-}
-
-class Grass : Entity
-{
-    
-    private int timeBeforeRespawn = Random.Range(1,5);
-    
-    
-}
 
 
